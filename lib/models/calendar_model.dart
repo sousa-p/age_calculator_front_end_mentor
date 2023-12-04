c2a4f8728418928972a755f892535bf47ef6636c
sousa-p <p.soousa7@gmail.com>
2023-12-04 08:45:12 -0300

    Adicionando métodos para calendar_model e adicionando arquivos de teste

--- lib/models/calendar_model.dart
+++ lib/models/calendar_model.dart
@@ -1,13 +1,17 @@
+import 'package:dart_date/dart_date.dart';
+
 class CalendarModel {
-  late final int mouth;
+  late final int month;
   late final int day;
   late final int year;
 
-  CalendarModel({required this.mouth, required this.day, required this.year});
+  CalendarModel({required this.month, required this.day, required this.year});
 
-  bool isValidMouth() => 0 < mouth && mouth < 13;
+  bool isValidMonth() => 0 < month && month < 13;
 
   bool isValidDay() => 0 < day && day < 32;
 
   bool isValidYear() => 0 < year;
+
+  bool isValidDate() => Date.isDate(DateTime(year, month, day));
 }
